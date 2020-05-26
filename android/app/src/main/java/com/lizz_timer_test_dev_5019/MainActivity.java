d74b9a26739850e20970674490215acadcf3d902
Crowdbotics <team@crowdbotics.com>
2020-05-26 20:51:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lizz_timer_test_dev_5019/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lizz_timer_test_dev_5019;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "lizz_timer_test_dev_5019";
+    }
+}
